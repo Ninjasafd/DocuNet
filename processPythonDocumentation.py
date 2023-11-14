936f3c751e6ad106633872662c8135bfbeef0b8a
Ninjasafd <74614776+Ninjasafd@users.noreply.github.com>
2023-11-14 02:23:12 -0600

    Half-working

--- processPythonDocumentation.py
+++ processPythonDocumentation.py
@@ -1,32 +1,49 @@
+import fitz  # PyMuPDF
 import re
+import spacy
 
-# This regex pattern matches lines that start with >>> or ...
-code_pattern = re.compile(r'^\s*(>>>|...).*$', re.MULTILINE)
-
-# Sample text from the OCR process
-sample_text = """
-4.9.5 Memory Views
-memoryview objects allow Python code to access the internal data of an object that supports the buffer protocol without copying.
-
-class memoryview(object)
-Create a memoryview that references object. object must support the buffer protocol. Built-in objects that support the buffer protocol include bytes and bytearray.
-
->>> v = memoryview(b'abcefg')
-98
->>> v[1]
-99
->>> v[-1]
-103
->>> v[1:4]
-memory at 0x7e3ddc9f4350>
->>> bytes(v[1:4])
-b'bce'
-"""
-
-# Remove code samples
-processed_text = re.sub(code_pattern, '', sample_text)
-
-# Output the processed text
-print(processed_text)
+# Open the PDF file
+with fitz.open("python_library.pdf") as doc:
+    text = ""
+    for page in doc:
+        # Extract text from each page
+        text += page.get_text()
+        # print(page.get_text())
 
 
+# Define a regex pattern for Python code blocks
+code_pattern = re.compile(r'>>>.*?(\n|$)')
+
+# Remove code blocks from the extracted text
+text_without_code = re.sub(code_pattern, '', text)
+
+
+# Load the language model
+nlp = spacy.load("en_core_web_sm")
+nlp.max_length = len(text_without_code) + 100
+
+# Process the cleaned text to segment into sentences
+doc = nlp(text_without_code)
+sentences = [sent.text.strip() for sent in doc.sents]
+
+# Assume sentences is a list of sentence strings
+refined_sentences = []
+
+def is_coherent_sentence(sentence):
+    # Define logic to determine if a sentence is coherent
+    # For example, a coherent sentence should start with a capital letter and end with a terminal punctuation mark
+    if sentence[0].isupper() and sentence.endswith(('.', '?', '!')):
+        return True
+    return False
+
+for sentence in sentences:
+    if is_coherent_sentence(sentence):
+        refined_sentences.append(sentence)
+
+
+
+
+filename = 'python_refined_sentences.txt'
+with open(filename, 'w', encoding='utf-8') as file:
+    for sentence in refined_sentences:
+        file.write(sentence + '\n')
